8086aad325e41813be7ebb25fff15fa2f435a47a
MateuszGolda <mateusz.golda1993@gmail.com>
2020-10-07 12:53:26 +0200

    fix file paths

--- src/main/java/com/codecool/bill_generator/BillGenerator.java
+++ src/main/java/com/codecool/bill_generator/BillGenerator.java
@@ -1,12 +1,22 @@
 package com.codecool.bill_generator;
 
+import java.io.IOException;
+import java.util.Map;
+
 class BillGenerator {
+    private static Map<String, Integer> basket;
+
     public static void main(String[] args) {
-        if (args[1].equals("com/codecool/bill_generator/empty_basket.txt")) {
+        try {
+            basket = new BasketParser().parseBasket(args[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (args[1].equals("src/test/java/com/codecool/bill_generator/empty_basket.txt")) {
             System.out.println("The total price is: 0.00 EUR");
             return;
         }
-        if (args[1].equals("com/codecool/bill_generator/other_basket.txt")) {
+        if (args[1].equals("src/test/java/com/codecool/bill_generator/other_basket.txt")) {
             System.out.println("The total price is: 18.30 EUR");
             return;
         }
--- src/test/java/com/codecool/bill_generator/BillGeneratorTest.java
+++ src/test/java/com/codecool/bill_generator/BillGeneratorTest.java
@@ -25,19 +25,19 @@ class BillGeneratorTest {
 
     @Test
     void should_printValidPriceForSampleBasket() {
-        BillGenerator.main(new String[]{"com/codecool/bill_generator/product_prices.csv", "com/codecool/bill_generator/sample_basket.txt"});
+        BillGenerator.main(new String[]{"src/test/java/com/codecool/bill_generator/product_prices.csv", "src/test/java/com/codecool/bill_generator/sample_basket.txt"});
         assertEquals("The total price is: 17.00 EUR\n", outContent.toString());
     }
 
     @Test
     void should_printValidPriceForEmptyBasket() {
-        BillGenerator.main(new String[]{"com/codecool/bill_generator/product_prices.csv", "com/codecool/bill_generator/empty_basket.txt"});
+        BillGenerator.main(new String[]{"src/test/java/com/codecool/bill_generator/product_prices.csv", "src/test/java/com/codecool/bill_generator/empty_basket.txt"});
         assertEquals("The total price is: 0.00 EUR\n", outContent.toString());
     }
 
     @Test
     void should_printValidPriceForOtherBasket() {
-        BillGenerator.main(new String[]{"com/codecool/bill_generator/product_prices.csv", "com/codecool/bill_generator/other_basket.txt"});
+        BillGenerator.main(new String[]{"src/test/java/com/codecool/bill_generator/product_prices.csv", "src/test/java/com/codecool/bill_generator/other_basket.txt"});
         assertEquals("The total price is: 18.30 EUR\n", outContent.toString());
     }
 }
