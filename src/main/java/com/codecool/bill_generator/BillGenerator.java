b20454d28f42f56c803c372c2fa9cabefedd21ca
MateuszGolda <mateusz.golda1993@gmail.com>
2020-10-08 07:20:03 +0200

    implement method computeBillTotal

--- src/main/java/com/codecool/bill_generator/BillGenerator.java
+++ src/main/java/com/codecool/bill_generator/BillGenerator.java
@@ -1,25 +1,40 @@
 package com.codecool.bill_generator;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class BillGenerator {
+    private static Map<String, List<AmountAndPrice>> products;
     private static Map<String, Integer> basket;
 
     public static void main(String[] args) {
         try {
+            products = new ProductParser().parseProducts(args[0]);
             basket = new BasketParser().parseBasket(args[1]);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (args[1].equals("src/test/java/com/codecool/bill_generator/empty_basket.txt")) {
-            System.out.println("The total price is: 0.00 EUR");
-            return;
-        }
-        if (args[1].equals("src/test/java/com/codecool/bill_generator/other_basket.txt")) {
-            System.out.println("The total price is: 18.30 EUR");
-            return;
-        }
-        System.out.println("The total price is: 17.00 EUR");
+        int total = computeBillTotal();
+        System.out.printf("The total price is: %.2f EUR\n", total / 100.);
+    }
+
+    private static int computeBillTotal() {
+        AtomicInteger total = new AtomicInteger();
+        basket.forEach((k, v) -> {
+            int i = 0;
+            int productsInBasket = v;
+            AmountAndPrice promotion;
+
+            while (i < products.get(k).size()) {
+                promotion = products.get(k).get(i);
+                if (productsInBasket >= promotion.getAmount()) {
+                    total.addAndGet(productsInBasket / promotion.getAmount() * promotion.getPrice());
+                    productsInBasket %= promotion.getAmount();
+                } else i++;
+            }
+        });
+        return total.get();
     }
 }
